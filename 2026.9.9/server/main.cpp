cf012101c03399bc76e31227d9dfb5e4897e7574
Nie <2862039182@sample.com>
2026-09-09 22:22:46 +0800

    增加逻辑队列，设计LogicSystem单例类

---
+++ 2026.9.9/server/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include "Session.h"
+#include "CServer.h"
+#include "MsgNode.h"
+int main()
+{
+	try {
+		boost::asio::io_context ioc;
+		using namespace std;
+		CServer s(ioc, 10086);
+		ioc.run();
+	}
+	catch (std::exception& e) {
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+	return 0;
+}
